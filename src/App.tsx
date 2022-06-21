0d82f789179d7a4957d2248c802073de8379b396
Rafael Rodrigo <rafael.rodrigo.p.a@gmail.com>
2022-06-21 20:16:06 -0300

    Lesson 01 Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,41 @@
+import { gql, useQuery } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string,
+  title: string
+}
+
 function App() {
+
+  const { data } = useQuery<{lessons: Lesson[]}>(GET_LESSONS_QUERY)
+  console.log(data);
+
+  // useEffect(() => {
+  //   client.query(
+  //       {
+  //         query: GET_LESSONS_QUERY,
+  //       }).then(response => {
+  //         console.log(response.data)
+  //       })
+  // },[])
+
   return (
-    <h1 className="text-5xl font-bold" >Hello word</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>
+      })}
+    </ul>
   )
 }
 
